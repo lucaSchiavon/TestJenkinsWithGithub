ada23579a8f8af45f6921b05aeefe252f9150869
Luca Schiavon <ext_l.schiavon@archivagroup.it>
2021-12-24 19:13:19 +0100

    aggiunte iii

--- TestJenkinsEFPipelines/TestJenkinsEFPipelines/Manager.cs
+++ TestJenkinsEFPipelines/TestJenkinsEFPipelines/Manager.cs
@@ -14,7 +14,7 @@ namespace TestJenkinsEFPipelines
             HelloWorldDTO helloWorldDto = new HelloWorldDTO()
             {
                 Message = "Hello world",
-                Other = "altro contenuto xxx ggg fff ooo and after fff"
+                Other = "altro contenuto xxx ggg fff ooo and after fff iii"
         };
             string json = JsonConvert.SerializeObject(helloWorldDto, Formatting.Indented);
             return json;
