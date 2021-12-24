6d7650d8e4773fd1808aed192748ab97d444e0fd
Luca Schiavon <ext_l.schiavon@archivagroup.it>
2021-12-24 16:45:24 +0100

    my first commit

---
+++ TestJenkinsEFPipelines/TestJenkinsEFPipelines.Test/TestJenkinsEFPipelines.Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestJenkinsEFPipelines.Test
+{
+    [TestClass]
+    public class TestJenkinsEFPipelinesTest
+    {
+        [TestMethod]
+        public void HelloWorld_GeneraUnOggettoJson_MiAspettoUnOggettoValorizzatoInUnCertoModo()
+        {
+            //arrange
+            Manager manager = new Manager();
+            //act
+            string s=manager.HelloWorld();
+            //assert
+            Assert.IsTrue(s.Contains("Hello world"));
+        }
+    }
+}
---
+++ TestJenkinsEFPipelines/TestJenkinsEFPipelines/Manager.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJenkinsEFPipelines
+{
+    public class Manager
+    {
+        public string HelloWorld()
+        {
+            HelloWorldDTO helloWorldDto = new HelloWorldDTO()
+            {
+                Message = "Hello world",
+                Other = "altro contenuto xxx ggg fff"
+        };
+            string json = JsonConvert.SerializeObject(helloWorldDto, Formatting.Indented);
+            return json;
+        }
+    }
+}
